9ec9bc5b942561f27f66f7f49ee79954911192b0
malabed1200 <malabed@student.tgm.ac.at>
2025-02-16 11:10:35 +0100

    Unnötiige Klasse gelöscht

--- Spellcraft/Spiel1.java
+++
@@ -1,9 +0,0 @@
-import MODEL.HangmanModel;
-import VIEW.HangmanView;
-
-public class Spiel1 {
-    public static void main(String[] args) {
-        HangmanModel model = new HangmanModel();
-        new HangmanView(model);
-    }
-}
